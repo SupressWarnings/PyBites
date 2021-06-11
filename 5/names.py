2b1589eb64ca79b617473aadbe79827c9ad12dfa
Constantin Schulte <c.schulte@accure.net>
2021-06-11 19:19:59 +0200

    submission Bite 5 @ codechalleng.es

--- 5/names.py
+++ 5/names.py
@@ -1,19 +1,34 @@
-NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
-         'julian sequeira', 'sandra bullock', 'keanu reeves',
-         'julbob pybites', 'bob belderbos', 'julian sequeira',
-         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+NAMES = [
+    "arnold schwarzenegger",
+    "alec baldwin",
+    "bob belderbos",
+    "julian sequeira",
+    "sandra bullock",
+    "keanu reeves",
+    "julbob pybites",
+    "bob belderbos",
+    "julian sequeira",
+    "al pacino",
+    "brad pitt",
+    "matt damon",
+    "brad pitt",
+]
 
 
 def dedup_and_title_case_names(names):
     """Should return a list of title cased names,
        each name appears only once"""
-    pass
+    title_names = [name.title() for name in names]
+    dedup = list(dict.fromkeys(title_names))
+    return dedup
 
 
 def sort_by_surname_desc(names):
     """Returns names list sorted desc by surname"""
     names = dedup_and_title_case_names(names)
     # ...
+    names.sort(key=lambda name: name.split()[-1])
+    return names
 
 
 def shortest_first_name(names):
@@ -22,3 +37,6 @@ def shortest_first_name(names):
     """
     names = dedup_and_title_case_names(names)
     # ...
+    names.sort(key=lambda name: name.split()[0])
+    return names[0].split()[0]
+
