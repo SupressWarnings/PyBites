4813c2b83f0acceb31ebb7d97cf146a16835676d
Constantin Schulte <c.schulte@accure.net>
2021-06-14 15:45:27 +0200

    submission Bite 46 @ codechalleng.es

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -1,2 +1,9 @@
 def fizzbuzz(num):
-    pass
+    if num % 3 != 0 and num % 5 != 0:
+        return num
+    solution = ""
+    if num % 3 == 0:
+        solution += "Fizz "
+    if num % 5 == 0:
+        solution += "Buzz"
+    return solution.strip(" ")
