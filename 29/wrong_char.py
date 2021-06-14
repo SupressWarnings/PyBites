3b75630fafde08c33118ac710579a8465d9b039c
Constantin Schulte <c.schulte@accure.net>
2021-06-14 14:20:07 +0200

    submission Bite 29 @ codechalleng.es

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -3,13 +3,13 @@ ALPHANUM = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 def get_index_different_char(chars):
     alnumerical = True
-    if chars[0] in ALPHANUM and chars[1] in ALPHANUM:
+    if str(chars[0]) in ALPHANUM and str(chars[1]) in ALPHANUM:
         alnumerical = True
-    elif chars[0] in ALPHANUM and chars[1] not in ALPHANUM:
+    elif str(chars[0]) in ALPHANUM and str(chars[1]) not in ALPHANUM:
         alnumerical = False
     else:
-        alnumerical = chars[0] in ALPHANUM and chars[2] in ALPHANUM
+        alnumerical = str(chars[0]) in ALPHANUM and str(chars[2]) in ALPHANUM
 
     for character in chars:
-        if (character not in ALPHANUM) == alnumerical:
+        if (str(character) not in ALPHANUM) == alnumerical:
             return chars.index(character)
