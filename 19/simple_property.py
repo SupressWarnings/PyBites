2d64e03cb4ce54c661b772c753919b7e8a90f9df
Constantin Schulte <c.schulte@accure.net>
2021-06-14 12:00:55 +0200

    submission Bite 19 @ codechalleng.es

--- 19/simple_property.py
+++ 19/simple_property.py
@@ -5,4 +5,15 @@ NOW = datetime.now()
 
 class Promo:
     def __init__(self, name, expires):
-        self.expired = expires < NOW
+        self._expired = expires < NOW
+
+    def getexpired(self):
+        return self._expired
+
+    def setexpired(self, new):
+        self._expired = new
+
+    def delexpired(self):
+        del self._expired
+
+    expired = property(getexpired, setexpired, delexpired)
