3b920d93b444cac20e47f434c5636ba0a5064eb3
Constantin Schulte <c.schulte@accure.net>
2021-06-14 14:59:38 +0200

    submission Bite 38 @ codechalleng.es

--- 43/kwargs.py
+++ 43/kwargs.py
@@ -1,2 +1,4 @@
-def get_profile(name="julian", profession="programmer"):
+def get_profile(*util, name="julian", profession="programmer"):
+    if util != None:
+        raise TypeError
     return f"{name} is a {profession}"
