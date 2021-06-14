0e9dc179756d7511ed87a39cdb8072da5b45a650
Constantin Schulte <c.schulte@accure.net>
2021-06-14 15:20:14 +0200

    submission Bite 44 @ codechalleng.es

--- 44/license.py
+++ 44/license.py
@@ -3,14 +3,13 @@ import string
 
 
 def gen_key(parts=4, chars_per_part=8):
-    solution = ""
-    for i in range(parts):
-        solution += "".join(
-            (
-                secrets.choice(string.ascii_uppercase + string.digits)
-                for i in range(chars_per_part)
-            )
+    return "-".join([gen_key_part(chars_per_part) for i in range(parts)])
+
+
+def gen_key_part(chars_per_part):
+    return "".join(
+        (
+            secrets.choice(string.ascii_uppercase + string.digits)
+            for i in range(chars_per_part)
         )
-        if i < parts - 1:
-            solution += "-"
-    return solution
+    )
