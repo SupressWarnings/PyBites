9a83210f7720544c57f4680e3d679344abc518fc
Constantin Schulte <c.schulte@accure.net>
2021-06-14 12:25:34 +0200

    submission Bite 21 @ codechalleng.es

--- 21/cars.py
+++ 21/cars.py
@@ -1,31 +1,44 @@
 cars = {
-    'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
-    'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
-    'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
-    'Honda': ['Civic', 'Accord', 'Odyssey', 'Jazz'],
-    'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
+    "Ford": ["Falcon", "Focus", "Festiva", "Fairlane"],
+    "Holden": ["Commodore", "Captiva", "Barina", "Trailblazer"],
+    "Nissan": ["Maxima", "Pulsar", "350Z", "Navara"],
+    "Honda": ["Civic", "Accord", "Odyssey", "Jazz"],
+    "Jeep": ["Grand Cherokee", "Cherokee", "Trailhawk", "Trackhawk"],
 }
 
 
 def get_all_jeeps(cars=cars):
     """return a comma  + space (', ') separated string of jeep models
        (original order)"""
-    pass
+    solution = ""
+    for jeep in cars["Jeep"]:
+        solution += jeep + ", "
+    return solution[:-2]
 
 
 def get_first_model_each_manufacturer(cars=cars):
     """return a list of matching models (original ordering)"""
-    pass
+    return [model_set[0] for model_set in cars.values()]
 
 
-def get_all_matching_models(cars=cars, grep='trail'):
+def get_all_matching_models(cars=cars, grep="trail"):
     """return a list of all models containing the case insensitive
        'grep' string which defaults to 'trail' for this exercise,
        sort the resulting sequence alphabetically"""
-    pass
+    grepped = [
+        model
+        for model_set in cars.values()
+        for model in model_set
+        if grep.lower() in model.lower()
+    ]
+    grepped.sort()
+    return grepped
 
 
 def sort_car_models(cars=cars):
     """return a copy of the cars dict with the car models (values)
        sorted alphabetically"""
     pass
+
+
+print(get_all_matching_models())
