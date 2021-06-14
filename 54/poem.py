0daed23017dd6bd7d4956a901b425b4366eb443f
Constantin Schulte <c.schulte@accure.net>
2021-06-14 16:05:56 +0200

    submission Bite 54 @ codechalleng.es

--- 54/poem.py
+++ 54/poem.py
@@ -3,9 +3,10 @@ INDENTS = 4
 
 def print_hanging_indents(poem):
     lines = poem.split("\n")
+    lines = [line.strip() for line in lines]
     for i in range(1, len(lines)):
         if lines[i - 1] != "" and lines[i] != "":
-            lines[i] = f"    {lines[i]}"
+            lines[i] = f"{INDENTS*' '}{lines[i]}"
     lines
     print("\n".join(list(filter("".__ne__, lines))))
     return
