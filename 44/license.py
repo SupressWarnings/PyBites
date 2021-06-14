f06e110dc41d1c5074c935782b3bbbe9cad2f362
Constantin Schulte <c.schulte@accure.net>
2021-06-14 15:15:14 +0200

    submission Bite 44 @ codechalleng.es

--- 44/license.py
+++ 44/license.py
@@ -1,1 +1,16 @@
-def gen_key(...
+import secrets
+import string
+
+
+def gen_key(parts=4, chars_per_part=8):
+    solution = ""
+    for i in range(parts):
+        solution += "".join(
+            (
+                secrets.choice(string.ascii_uppercase + string.digits)
+                for i in range(chars_per_part)
+            )
+        )
+        if i < parts - 1:
+            solution += "-"
+    return solution
