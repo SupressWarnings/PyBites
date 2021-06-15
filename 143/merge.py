d927445758b1fa30113d68c5b98764099dac5e21
Constantin Schulte <c.schulte@accure.net>
2021-06-15 15:26:11 +0200

    submission Bite 143 @ codechalleng.es

--- 143/merge.py
+++ 143/merge.py
@@ -1,8 +1,8 @@
 NOT_FOUND = "Not found"
 
-group1 = {'tim': 30, 'bob': 17, 'ana': 24}
-group2 = {'ana': 26, 'thomas': 64, 'helen': 26}
-group3 = {'brenda': 17, 'otto': 44, 'thomas': 46}
+group1 = {"tim": 30, "bob": 17, "ana": 24}
+group2 = {"ana": 26, "thomas": 64, "helen": 26}
+group3 = {"brenda": 17, "otto": 44, "thomas": 46}
 
 
 def get_person_age(name):
@@ -10,4 +10,15 @@ def get_person_age(name):
        If name in > 1 dict, return the match of the group with
        greatest N (so group3 > group2 > group1)
     """
-    pass
+    if type(name) is not str:
+        return "Not found"
+    keyword = name.lower()
+    if keyword in group3.keys():
+        return group3[keyword]
+    elif keyword in group2.keys():
+        return group2[keyword]
+    elif keyword in group1.keys():
+        return group1[keyword]
+    else:
+        return "Not found"
+
