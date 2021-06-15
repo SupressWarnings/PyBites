6231763c98bd2a08176621adea2ff6a2e4f0c304
Constantin Schulte <c.schulte@accure.net>
2021-06-15 11:59:50 +0200

    submission Bite 74 @ codechalleng.es

--- 74/bday.py
+++ 74/bday.py
@@ -1,6 +1,16 @@
 import calendar
 
+WEEKDAYS = (
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+)
+
 
 def weekday_of_birth_date(date):
     """Takes a date object and returns the corresponding weekday string"""
-    pass
+    return WEEKDAYS[calendar.weekday(date.year, date.month, date.day)]
