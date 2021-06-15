8628d841352211d1cbe7a9372d200f8c4cf38616
Constantin Schulte <c.schulte@accure.net>
2021-06-15 17:17:26 +0200

    submission Bite 241 @ codechalleng.es

--- 241/test_numbers_to_dec.py
+++ 241/test_numbers_to_dec.py
@@ -1,3 +1,33 @@
 import pytest
 
 from numbers_to_dec import list_to_decimal
+
+
+def test_single():
+    assert list_to_decimal([1]) == 1
+    assert list_to_decimal([0]) == 0
+
+
+def test_multiple():
+    assert list_to_decimal([1, 1]) == 11
+    assert list_to_decimal([0, 1, 1]) == 11
+
+
+def test_float():
+    with pytest.raises(TypeError):
+        list_to_decimal([0.5])
+
+
+def test_string():
+    with pytest.raises(TypeError):
+        list_to_decimal(["a"])
+
+
+def test_bool():
+    with pytest.raises(TypeError):
+        list_to_decimal([True])
+
+
+def test_negative():
+    with pytest.raises(ValueError):
+        list_to_decimal([-1])
