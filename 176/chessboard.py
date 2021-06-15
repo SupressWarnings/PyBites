94ad3f114cf92613a62f3edec1449716b67a6c3f
Constantin Schulte <c.schulte@accure.net>
2021-06-15 16:12:29 +0200

    submission Bite 176 @ codechalleng.es

--- 176/chessboard.py
+++ 176/chessboard.py
@@ -1,7 +1,9 @@
-WHITE, BLACK = ' ', '#'
+WHITE, BLACK = " ", "#"
 
 
 def create_chessboard(size=8):
     """Create a chessboard with of the size passed in.
        Don't return anything, print the output to stdout"""
-    pass
+    for i in range(size):
+        print((f"{WHITE}{BLACK}" if i % 2 == 0 else f"{BLACK}{WHITE}") * int(size / 2))
+
