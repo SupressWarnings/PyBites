289a8f200e0b5bfb32c3b22a55c56ee1d72aeae2
Constantin Schulte <c.schulte@accure.net>
2021-06-15 11:07:11 +0200

    submission Bite 66 @ codechalleng.es

--- 66/running_mean.py
+++ 66/running_mean.py
@@ -1,5 +1,12 @@
+import itertools
+
+
 def running_mean(sequence):
     """Calculate the running mean of the sequence passed in,
        returns a sequence of same length with the averages.
        You can assume all items in sequence are numeric."""
-    pass
+    n = 1
+    iter_sequence = itertools.accumulate(sequence)
+    for sum in iter_sequence:
+        yield round(sum / n, 2)
+        n += 1
