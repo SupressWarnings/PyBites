32310901f2f405958f8ca551aa5190ec1ddce5d1
Constantin Schulte <c.schulte@accure.net>
2021-06-15 14:17:43 +0200

    submission Bite 117 @ codechalleng.es

--- 117/round_even.py
+++ 117/round_even.py
@@ -1,3 +1,6 @@
 def round_even(number):
     """Takes a number and returns it rounded even"""
-    pass
+    if number % 1 != 0.5:
+        return round(number)
+    else:
+        return int(number / 1) if number % 2 < 1 else int(number / 1 + 1)
