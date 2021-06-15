e9f7aa85dab1bf7e1c8dd25c76809d22bf40834a
Constantin Schulte <c.schulte@accure.net>
2021-06-15 12:56:01 +0200

    submission Bite 96 @ codechalleng.es

--- 96/wc.py
+++ 96/wc.py
@@ -1,12 +1,20 @@
+import re
+
+
 def wc(file_):
     """Takes an absolute file path/name, calculates the number of
        lines/words/chars, and returns a string of these numbers + file, e.g.:
        3 12 60 /tmp/somefile
        (both tabs and spaces are allowed as separator)"""
-    pass
+    file = open(file_).read()
+    lines = len(file.splitlines())
+    words = len(list(filter(None, re.split("[ \\\\/:|.\\\n]", file))))
+    chars = len(file)
+    return "\t".join((str(lines), str(words), str(chars), str(file_)))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # make it work from cli like original unix wc
     import sys
+
     print(wc(sys.argv[1]))
