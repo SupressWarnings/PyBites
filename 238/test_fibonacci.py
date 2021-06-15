b270c44e930b5a0561ce9e1397be1890e25c64b1
Constantin Schulte <c.schulte@accure.net>
2021-06-15 17:04:48 +0200

    submission Bite 238 @ codechalleng.es

--- 238/test_fibonacci.py
+++ 238/test_fibonacci.py
@@ -1,3 +1,18 @@
 from fibonacci import fib
+import pytest
 
 # write one or more pytest functions below, they need to start with test_
+def test_first():
+    assert fib(0) == 0
+    assert fib(1) == 1
+
+
+def test_later():
+    assert fib(2) == 1
+    assert fib(3) == 2
+    assert fib(10) == 55
+
+
+def test_negative():
+    with pytest.raises(ValueError):
+        fib(-1)
