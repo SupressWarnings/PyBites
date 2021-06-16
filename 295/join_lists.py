6af19ee7b29fcb8d43a6fd2f8007ec03e61ffc1f
Constantin Schulte <c.schulte@accure.net>
2021-06-16 12:13:41 +0200

    submission Bite 295 @ codechalleng.es

--- 295/join_lists.py
+++ 295/join_lists.py
@@ -2,4 +2,9 @@ from typing import List, Union
 
 
 def join_lists(lst_of_lst: List[List[str]], sep: str) -> Union[List[str], None]:
-    ...
+    if lst_of_lst is None or len(lst_of_lst) == 0:
+        return None
+    union = []
+    for lst in lst_of_lst:
+        union += lst + [sep]
+    return union[0:-1]
