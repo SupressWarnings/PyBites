ba30731f29f495c823ad675c8ab6cb8537c23edd
Constantin Schulte <c.schulte@accure.net>
2021-06-16 12:02:31 +0200

    submission Bite 270 @ codechalleng.es

--- 270/freq.py
+++ 270/freq.py
@@ -1,2 +1,7 @@
+from collections import Counter
+
+
 def freq_digit(num: int) -> int:
-    pass
+    num_list = list(str(num))
+    sorted_counter = Counter(num_list).most_common()
+    return int(sorted_counter[0][0])
