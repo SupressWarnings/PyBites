1436628ecbe0551ad2ef84f0c9318f85e0e8a7d2
Constantin Schulte <c.schulte@accure.net>
2021-06-16 11:22:04 +0200

    submission Bite 252 @ codechalleng.es

--- 252/series.py
+++ 252/series.py
@@ -7,53 +7,51 @@ def return_at_index(ser: pd.Series, idx: int) -> object:
     If you want to be extra careful catch and raise an error if
        the index does not exist.
     """
-    pass
+    return ser[idx]
 
 
 def get_slice(ser: pd.Series, start: int, end: int) -> pd.core.series.Series:
     """Return the slice of the given Series in the range between
     start and end.
     """
-    pass
+    return ser[start:end]
 
 
-def get_slice_inclusive(ser: pd.Series,
-                        start: int, end: int) -> pd.core.series.Series:
+def get_slice_inclusive(ser: pd.Series, start: int, end: int) -> pd.core.series.Series:
     """Return the slice of the given Series in the range between
     start and end inclusive.
     """
-    pass
+    return ser.iloc[start : end + 1]
 
 
 def return_head(ser: pd.Series, num: int) -> pd.core.series.Series:
     """Return the first num elements of the given Series.
     """
-    pass
+    return ser.head(num)
 
 
 def return_tail(ser: pd.Series, num: int) -> pd.core.series.Series:
     """Return the last num elements of the given Series.
     """
-    pass
+    return ser.tail(num)
 
 
 def get_index(ser: pd.Series) -> pd.core.indexes.base.Index:
     """Return all indexes of the given Series.
     """
-    pass
+    return ser.index
 
 
 def get_values(ser: pd.Series) -> np.ndarray:
     """Return all the values of the given Series.
     """
-    pass
+    return ser.to_numpy()
 
 
-def get_every_second_indexes(ser: pd.Series,
-                             even_index=True) -> pd.core.series.Series:
+def get_every_second_indexes(ser: pd.Series, even_index=True) -> pd.core.series.Series:
     """Return all rows where the index is either even or odd.
     If even_index is True return every index where idx % 2 == 0
     If even_index is False return every index where idx % 2 != 0
     Assume default indexing i.e. 0 -> n
     """
-    pass
+    return ser.loc[ser.index % 2 != even_index]
