1e3df65b431d49de3789b8560a7f07a74787088c
Constantin Schulte <c.schulte@accure.net>
2021-06-21 16:27:08 +0200

    submission Bite 9 @ codechalleng.es

--- 9/palindrome.py
+++ 9/palindrome.py
@@ -2,9 +2,10 @@
 which reads the same backward as forward"""
 import os
 import urllib.request
+import re
 
-DICTIONARY = os.path.join('/tmp', 'dictionary_m_words.txt')
-urllib.request.urlretrieve('http://bit.ly/2Cbj6zn', DICTIONARY)
+DICTIONARY = os.path.join("/tmp", "dictionary_m_words.txt")
+urllib.request.urlretrieve("http://bit.ly/2Cbj6zn", DICTIONARY)
 
 
 def load_dictionary():
@@ -18,11 +19,13 @@ def is_palindrome(word):
        Case insensitive, so Madam is valid too.
        It should work for phrases too so strip all but alphanumeric chars.
        So "No 'x' in 'Nixon'" should pass (see tests for more)"""
-    pass
+    word = re.sub(r"[^a-z]", "", word.lower())
+    return word == word[::-1]
 
 
 def get_longest_palindrome(words=None):
     """Given a list of words return the longest palindrome
        If called without argument use the load_dictionary helper
        to populate the words list"""
-    pass
+    palindromes = [word for word in words if is_palindrome(word)]
+    return max(palindromes, key=lambda x: len(x))
