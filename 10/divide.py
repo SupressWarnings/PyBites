0fcf611fa8e96f5c44a38dc37d0e10264f1b17f0
Constantin Schulte <c.schulte@accure.net>
2021-06-21 16:34:00 +0200

    submission Bite 10 @ codechalleng.es

--- 10/divide.py
+++ 10/divide.py
@@ -1,2 +1,11 @@
 def positive_divide(numerator, denominator):
-    pass
+    result = 0.0
+    try:
+        result = numerator / denominator
+    except TypeError:
+        raise TypeError
+    except ZeroDivisionError:
+        return 0
+    if result < 0:
+        raise ValueError
+    return result
