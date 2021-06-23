986aef523127f75bb3ea4f2ebd721c782b89974b
Constantin Schulte <c.schulte@accure.net>
2021-06-23 19:46:54 +0200

    submission Bite 12 @ codechalleng.es

--- 12/validate.py
+++ 12/validate.py
@@ -1,15 +1,42 @@
 from collections import namedtuple
 
-User = namedtuple('User', 'name role expired')
-USER, ADMIN = 'user', 'admin'
-SECRET = 'I am a very secret token'
+User = namedtuple("User", "name role expired")
+USER, ADMIN = "user", "admin"
+SECRET = "I am a very secret token"
 
-julian = User(name='Julian', role=USER, expired=False)
-bob = User(name='Bob', role=USER, expired=True)
-pybites = User(name='PyBites', role=ADMIN, expired=False)
+julian = User(name="Julian", role=USER, expired=False)
+bob = User(name="Bob", role=USER, expired=True)
+pybites = User(name="PyBites", role=ADMIN, expired=False)
 USERS = (julian, bob, pybites)
 
 # define exception classes here
 
+
+class UserDoesNotExist(RuntimeError):
+    def __init__(self, *arg):
+        self.args = arg
+
+
+class UserAccessExpired(RuntimeError):
+    def __init__(self, *arg):
+        self.args = arg
+
+
+class UserNoPermission(RuntimeError):
+    def __init__(self, *arg):
+        self.args = arg
+
+
 def get_secret_token(username):
-    pass
+    user_list = {user.name: user for user in USERS}
+    if username in user_list.keys():
+        user = user_list[username]
+        if user.expired:
+            raise UserAccessExpired
+        else:
+            if user.role == ADMIN:
+                return SECRET
+            else:
+                raise UserNoPermission
+    else:
+        raise UserDoesNotExist
