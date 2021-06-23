9fb04f7d6f74aa74da39178da0667ea998e048a0
Constantin Schulte <c.schulte@accure.net>
2021-06-23 20:09:29 +0200

    submission Bite 18 @ codechalleng.es

--- 18/harry.py
+++ 18/harry.py
@@ -1,19 +1,25 @@
 import os
 import urllib.request
+import re
 
 # data provided
 tmp = os.getenv("TMP", "/tmp")
-stopwords_file = os.path.join(tmp, 'stopwords')
-harry_text = os.path.join(tmp, 'harry')
+stopwords_file = os.path.join(tmp, "stopwords")
+harry_text = os.path.join(tmp, "harry")
 urllib.request.urlretrieve(
-    'https://bites-data.s3.us-east-2.amazonaws.com/stopwords.txt',
-    stopwords_file
+    "https://bites-data.s3.us-east-2.amazonaws.com/stopwords.txt", stopwords_file
 )
 urllib.request.urlretrieve(
-    'https://bites-data.s3.us-east-2.amazonaws.com/harry.txt',
-    harry_text
+    "https://bites-data.s3.us-east-2.amazonaws.com/harry.txt", harry_text
 )
 
 
 def get_harry_most_common_word():
-    pass
+    text = open(harry_text, encoding="utf-8").read()
+    stopwords = open(stopwords_file).read().split("\n")
+    words = re.split(" |\n", text)
+    words = [word.lower() for word in words]
+    words = [re.sub("[^a-z]", "", word) for word in words]
+    text_no_stop = [word for word in words if word not in stopwords]
+    freq_word = max(set(text_no_stop), key=text_no_stop.count)
+    return (freq_word, words.count(freq_word))
