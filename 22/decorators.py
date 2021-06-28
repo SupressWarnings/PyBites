b3d65ab8a6397eb2231c8fa3be680dfb72cd5654
Constantin Schulte <c.schulte@accure.net>
2021-06-28 11:18:04 +0200

    submission Bite 22 @ codechalleng.es

--- 22/decorators.py
+++ 22/decorators.py
@@ -2,4 +2,11 @@ from functools import wraps
 
 
 def make_html(element):
-    pass
+    def real_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return f"<{element}>{func(*args, **kwargs)}</{element}>"
+
+        return wrapper
+
+    return real_decorator
